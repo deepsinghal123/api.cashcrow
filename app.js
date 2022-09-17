a61aa6151283762f4633ad18f5da1e0af2dad6d5
Akshaykumarsinghal <59149089+Akshaykumarsinghal@users.noreply.github.com>
2022-09-17 18:05:11 +0530

    commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express=require('express')
-const PORT=3001
+const PORT=3000
 const app=express()
 const cors=require('cors')
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
